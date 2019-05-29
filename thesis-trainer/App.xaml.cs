9f1b8c245d66e5280386671e35816cfd490e2711
Diego Orellana <djorellanab@correo.url.edu.gt>
2019-05-29 12:13:14 -0600

    Agregar archivos de proyecto.

---
+++ thesis-trainer/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace thesis_trainer
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
